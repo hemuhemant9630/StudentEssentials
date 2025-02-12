ad9db779092b397c7dd03bfe9100ccaefedbd7a5
ratan221 <ratankumargupta685@gmail.com>
2025-02-13 00:48:52 +0530

    New files

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+//config env
+dotenv.config();
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send("<h1>welcome to mern project by HRH</h1>");
+});
+
+//port
+const PORT = process.env.PORT || 5050;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.bgCyan.white);
+});
+
