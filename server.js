563bd1bfdb2152ff966e20d7da2c24acee72b7fd
hemuhemant9630 <hemant_raghuwanshi@srmap.edu.in>
2025-02-13 00:58:49 +0530

    till video 3

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
 
 //config env
 dotenv.config();
 
+//database config
+connectDB();
+
 const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/', (req,res) => {
     res.send("<h1>welcome to mern project by HRH</h1>");
